6c9fcd1788cb0389e2ed275ddc3d0b8c1387fd3f
ssvkavin <ssvkavin@gmail.com>
2025-03-29 18:32:08 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -8,8 +8,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 CORS(app, resources={r"/*":{"origins": "*"}})
-#CORS(app)
-#CORS(app,origins=["http://localhost:3000"])
 app.register_blueprint(event_bp)
 
 if __name__ == "_main_":
